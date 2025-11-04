aa8f41cf820524770866e3eb0988c90cda5abd2b
SuryaKiran namala <suryakiran.namala@gmail.com>
2025-11-04 13:35:40 +0530

    Test
    test

---
+++ DemoSample.java
@@ -0,0 +1,34 @@
+class DemoSample{
+//variables/ data members
+//methods/ member functions//
+// objects ---
+
+/*prime number -- jfnjvfnfkjnn
+*/
+ public static void main(String[] args){
+	 
+	
+System.out.println("Hello CODEBEGUN....!");	
+	 
+ }
+ 
+ 
+ static{
+	 
+	 System.out.println("Hello CODEBEGUN....STATIC!");
+ }
+
+
+}
+
+class DemoSample2{
+//variables/ data members
+//methods/ member functions//
+// objects ---
+
+/*prime number -- jfnjvfnfkjnn
+*/
+ 
+
+
+}
